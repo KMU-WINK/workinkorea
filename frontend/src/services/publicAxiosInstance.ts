e216f8513500e3e5370c59c4466e16938407c88f
Jiwon Choi <jwchoi@lablup.com>
2024-07-20 18:17:29 +0900

    feat: custom axios instance 추가

---
+++ frontend/src/services/publicAxiosInstance.ts
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+const PublicAxiosInstance = axios.create({
+  baseURL: process.env.NEXT_BULIC_API_BASE_URI,
+  timeout: 1000,
+  headers: { 'Content-Type': 'application/json' },
+});
+
+export default PublicAxiosInstance;
