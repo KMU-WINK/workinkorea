a0ae1541b6c821e7f848230aab083857a45c4bec
Jiwon Choi <jwchoi@lablup.com>
2024-07-21 17:01:32 +0900

    feat: 카카오 토큰 발급 API 연결
    - 클라이언트에서 서버 측으로 인가 코드를 통하여 카카오 토큰을 발급하는 API 연결
    - 인가 코드를 통한 토큰 발급은 1회성이기 때문에 useRef를 이용하여 여러 번 요청이 가지 않도록 처리

--- frontend/src/app/signin/kakao/page.tsx
+++ frontend/src/app/signin/kakao/page.tsx
@@ -2,17 +2,32 @@
 
 import { authorizeWithKakao } from '@/services/auth/kakao';
 import { useSearchParams } from 'next/navigation';
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 
 const KakaoCallbackPage = () => {
   const searchParams = useSearchParams();
   const code = searchParams.get('code');
+  /**
+   * @description 카카오 액세스 토큰 요청 시, 발생하는 'KOE320 invalid_grant' 에레 방지용
+   * / 동일한 인가 코드를 두 번 이상 사용하지 못하도록 useRef를 flag처럼 사용합니다.
+   */
+  const isGranted = useRef<boolean>(false);
 
   useEffect(() => {
-    if (code) {
-      const response = authorizeWithKakao({ code: code });
+    if (code && !isGranted.current) {
+      const response = authorizeWithKakao({
+        clientId: process.env.NEXT_PUBLIC_KAKAO_JAVASCRIPT_KEY || '',
+        redirectUrl: `${window.location.protocol}//${window.location.host}/signin/kakao`,
+        code: code,
+      });
+
+      // 인가 코드(code)를 통하여 액세스 토큰 요청을 완료하였으므로 isGranted.current를 갱신합니다.
+      isGranted.current = true;
+
+      console.log(response);
+      // 해당 access token를 이용하여 서비스 서버로 API 요청 후, 회원가입 or 로그인 진행 로직 추가 예정입니다.
     }
-  }, [code]);
+  }, []);
 
   return <>콜백 페이지</>;
 };
--- frontend/src/services/auth/kakao.ts
+++ frontend/src/services/auth/kakao.ts
@@ -1,10 +1,22 @@
 import PublicAxiosInstance from '../publicAxiosInstance';
 
 type TAuthorizeWithKakao = {
+  clientId: string;
+  redirectUrl: string;
   code: string;
 };
 
-export const authorizeWithKakao = async ({ code }: TAuthorizeWithKakao) => {
-  const response = await PublicAxiosInstance.post('/auth/kakao', { code });
+export const authorizeWithKakao = async ({
+  clientId,
+  redirectUrl,
+  code,
+}: TAuthorizeWithKakao) => {
+  const response = await PublicAxiosInstance.get('/auth/kakao', {
+    params: {
+      client_id: clientId,
+      redirect_url: redirectUrl,
+      code: code,
+    },
+  });
   return response;
 };
