6c6ca2a6738788c7887a1322c2e7718d5b3d718a
jisung <cham0810@kookmin.ac.kr>
2024-07-26 19:25:30 +0900

    chore: tailwind font, colors 세팅

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -7,8 +7,27 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    fontSize: {
+      xxs: '0.625rem',
+      xs: '0.75rem',
+      sm: '0.875rem',
+      md: '1rem',
+      lg: '1.125rem',
+      xl: '1.25rem',
+    },
     colors: {
       main: '#33C4A8',
+      secondary: '#EAF9F6',
+      unavailable: '#C1DBD7',
+      wish: '#FF96A3',
+      kakao: '#FEE502',
+      naver: '#04C759',
+      gray: {
+        '1': '#F5F5F5',
+        '2': '#D9D9D9',
+        '3': '#A9A9A9',
+        '4': '#5A5A5A',
+      },
     },
     extend: {
       backgroundImage: {
