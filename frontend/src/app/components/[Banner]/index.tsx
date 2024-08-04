6cf07af141121f9d2dca69ad6f36edf7bdfab0e3
Vargun <heegun4690@naver.com>
2024-08-04 19:10:12 +0900

    chore: 변수 수정

--- frontend/src/app/components/[Banner]/index.tsx
+++ frontend/src/app/components/[Banner]/index.tsx
@@ -11,27 +11,27 @@ interface BannerProps {
 export default function Banner({ type, title, description }: BannerProps) {
   const commonStyle = 'flex flex-col gap-5 rounded-xl px-6 py-8 min-h-[360px]';
 
-  let typeClass = '';
+  let typeStyle = '';
 
   switch (type) {
     case 'white-filter-on':
-      typeClass = 'bg-[#ffffff1a] text-[#ffffff]';
+      typeStyle = 'bg-[#ffffff1a] text-[#ffffff]';
       break;
     case 'white-filter-off':
-      typeClass = 'text-white';
+      typeStyle = 'text-white';
       break;
     case 'black-filter-on':
-      typeClass = 'bg-[#0000001a] text-[#000000]';
+      typeStyle = 'bg-[#0000001a] text-[#000000]';
       break;
     case 'black-filter-off':
-      typeClass = 'text-[#000000]';
+      typeStyle = 'text-[#000000]';
       break;
     default:
       break;
   }
 
   return (
-    <div className={`${commonStyle} ${typeClass}`}>
+    <div className={`${commonStyle} ${typeStyle}`}>
       {/* 24px -> xxl 로 수정 예정 */}
       <div className="text-[24px] font-semibold">{title}</div>
       <div>{description}</div>
