63c419e9a2c68da9b7c2db4891a40ccdc5122ce2
daun-up <awhekdns@kookmin.ac.kr>
2024-08-05 14:45:52 +0900

    feat: button-component ui 구현

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,24 @@
+'use client';
+
 import Image from 'next/image';
+import Button from './components/Button/index';
+import SelectButton from './components/Button/select';
+
+const handleClick = () => {
+  alert('버튼 클릭!');
+};
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between w-[393px]">
+      <Button onClick={handleClick} isSelect text="버튼입니다." />
+      <Button onClick={handleClick} isSelect={false} text="버튼입니다." />
+      <SelectButton onClick={handleClick} isSelect text="select 버튼입니다." />
+      <SelectButton
+        onClick={handleClick}
+        isSelect={false}
+        text="select 버튼입니다."
+      />
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
