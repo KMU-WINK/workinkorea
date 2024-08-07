7bb26ebb72ea88b0308980d118902b28495d033e
Vargun <heegun4690@naver.com>
2024-08-07 22:14:46 +0900

    feat: key event 영역 수정

--- frontend/src/app/sign-in/_components/Modal.tsx
+++ frontend/src/app/sign-in/_components/Modal.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
 export default function Modal({
   children,
@@ -13,20 +13,28 @@ export default function Modal({
   onClose: () => void;
   handleKeyDown: (e: React.KeyboardEvent<HTMLDivElement>) => void;
 }) {
+  const modalRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (isOpen && modalRef.current) {
+      modalRef.current.focus();
+    }
+  }, [isOpen]);
+
   return (
     <div
       className={`${isOpen ? 'fixed z-50 inset-0 overflow-y-auto min-w-full min-h-screen' : 'hidden'}`}
-      role="dialog"
-      tabIndex={-1}
       aria-modal="true"
+      ref={modalRef}
+      tabIndex={-1}
     >
       <div
         className="fixed inset-0 bg-[#000000] bg-opacity-30"
         onClick={onClose}
         role="button"
         aria-label="Close modal"
-        onKeyDown={handleKeyDown}
         tabIndex={0}
+        onKeyDown={handleKeyDown}
       />
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-10">
         {children}
