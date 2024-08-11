8499f910716db189b3f9e3ccef3c7abcaa9761ab
Jiwon Choi <jwchoi@lablup.com>
2024-08-11 18:46:28 +0900

    fix: argument 전달 오류 수정

--- frontend/src/services/auth/kakao.ts
+++ frontend/src/services/auth/kakao.ts
@@ -6,17 +6,19 @@ type TAuthorizeWithKakao = {
   code: string;
 };
 
-const authorizeWithKakao =
-  () =>
-  async ({ clientId, redirectUrl, code }: TAuthorizeWithKakao) => {
-    const response = await PublicAxiosInstance.get('/auth/kakao', {
-      params: {
-        client_id: clientId,
-        redirect_url: redirectUrl,
-        code,
-      },
-    });
-    return response;
-  };
+const authorizeWithKakao = async ({
+  clientId,
+  redirectUrl,
+  code,
+}: TAuthorizeWithKakao) => {
+  const response = await PublicAxiosInstance.get('/auth/kakao', {
+    params: {
+      client_id: clientId,
+      redirect_url: redirectUrl,
+      code,
+    },
+  });
+  return response;
+};
 
 export default authorizeWithKakao;
