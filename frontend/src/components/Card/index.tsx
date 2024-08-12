3515e64454f19e8af12f6a027dd3143b61505f84
jisung <cham0810@kookmin.ac.kr>
2024-08-12 14:46:13 +0900

    fix: 리뷰 반영, heart svg 변경

--- frontend/src/components/Card/index.tsx
+++ frontend/src/components/Card/index.tsx
@@ -1,45 +1,44 @@
 'use client';
 
 import Image from 'next/image';
-import priceToString from '@/utils/priceToString';
 import styled from 'styled-components';
 import { CardProps, CardType, ServiceType } from '@/types/type';
 import Heart from '../../../public/svgs/heart.svg';
-import FullHeart from '../../../public/svgs/heart-fill.svg';
+import ColorHeart from '../../../public/svgs/heart-color.svg';
 
-const Container = styled.div<{ cardType: CardType }>`
+const Container = styled.div<{ $cardType: CardType }>`
   display: flex;
-  flex-direction: ${props => (props.cardType === 'map' ? 'row' : 'column')};
+  flex-direction: ${props => (props.$cardType === 'map' ? 'row' : 'column')};
   background-color: white;
   border: 1px solid #d9d9d9;
   border-radius: 10px;
   cursor: pointer;
-  height: ${props => (props.cardType === 'map' ? '145px' : '237px')};
+  height: ${props => (props.$cardType === 'map' ? '145px' : '237px')};
   overflow: hidden;
 `;
 
 const ImageSection = styled.div<{
-  cardType: CardType;
+  $cardType: CardType;
 }>`
   display: flex;
   justify-content: center;
   align-items: center;
-  width: ${props => (props.cardType === 'map' ? '9rem' : '100%')};
+  width: ${props => (props.$cardType === 'map' ? '9rem' : '100%')};
   border-color: #d9d9d9;
   border-width: ${props =>
-    props.cardType === 'map' ? '0 1px 0 0' : '0 0 1px 0'};
+    props.$cardType === 'map' ? '0 1px 0 0' : '0 0 1px 0'};
   position: relative;
-  flex: ${props => (props.cardType === 'map' ? '' : '1')};
+  flex: ${props => (props.$cardType === 'map' ? '' : '1')};
 `;
 
-const Content = styled.div<{ cardType: CardType }>`
+const Content = styled.div<{ $cardType: CardType }>`
   padding: 1rem 0.875rem;
   display: flex;
   flex-direction: column;
-  flex: ${props => (props.cardType === 'map' ? '1' : '')};
+  flex: ${props => (props.$cardType === 'map' ? '1' : '')};
   justify-content: ${props =>
-    props.cardType === 'map' ? 'space-between' : 'flex-start'};
-  gap: ${props => (props.cardType === 'map' ? '0' : '0.75rem')};
+    props.$cardType === 'map' ? 'space-between' : 'flex-start'};
+  gap: ${props => (props.$cardType === 'map' ? '0' : '0.75rem')};
 `;
 
 const DetailContainer = styled.div`
@@ -55,25 +54,25 @@ const Detail = styled.div`
   gap: 0.5rem;
 `;
 
-const Title = styled.p<{ cardType: CardType }>`
+const Title = styled.p<{ $cardType: CardType }>`
   font-weight: 600;
-  font-size: ${props => (props.cardType === 'map' ? '0.875rem' : '1rem')};
+  font-size: ${props => (props.$cardType === 'map' ? '0.875rem' : '1rem')};
   display: -webkit-box;
   -webkit-box-orient: vertical;
-  -webkit-line-clamp: ${props => (props.cardType === 'map' ? '2' : '1')};
+  -webkit-line-clamp: ${props => (props.$cardType === 'map' ? '2' : '1')};
   overflow: hidden;
   text-overflow: ellipsis;
 `;
 
 const InfoContainer = styled.div<{
-  cardType: CardType;
-  serviceType: ServiceType;
+  $cardType: CardType;
+  $serviceType: ServiceType;
 }>`
   display: flex;
   flex-direction: ${props =>
-    props.cardType === 'map' ? 'column-reverse' : 'row'};
+    props.$cardType === 'map' ? 'column-reverse' : 'row'};
   gap: ${props =>
-    props.cardType === 'default' && props.serviceType === 'default'
+    props.$cardType === 'default' && props.$serviceType === 'default'
       ? '0'
       : '9px'};
 `;
@@ -83,13 +82,13 @@ const BottomContainer = styled.div`
   justify-content: space-between;
 `;
 
-const Price = styled.p<{ cardType: CardType }>`
-  font-size: ${props => (props.cardType === 'map' ? '0.875rem' : '0.75rem')};
-  font-weight: ${props => (props.cardType === 'map' ? '500' : '400')};
+const Price = styled.p<{ $cardType: CardType }>`
+  font-size: ${props => (props.$cardType === 'map' ? '0.875rem' : '0.75rem')};
+  font-weight: ${props => (props.$cardType === 'map' ? '500' : '400')};
 `;
 
-const Location = styled.p<{ cardType: CardType }>`
-  font-size: ${props => (props.cardType === 'map' ? '0.625rem' : '0.75rem')};
+const Location = styled.p<{ $cardType: CardType }>`
+  font-size: ${props => (props.$cardType === 'map' ? '0.625rem' : '0.75rem')};
 `;
 
 export default function Card({
@@ -103,8 +102,8 @@ export default function Card({
   company = '',
 }: CardProps) {
   return (
-    <Container cardType={cardType}>
-      <ImageSection cardType={cardType}>
+    <Container $cardType={cardType}>
+      <ImageSection $cardType={cardType}>
         <Image
           src={image}
           alt="example"
@@ -116,31 +115,35 @@ export default function Card({
           }
         />
       </ImageSection>
-      <Content cardType={cardType}>
+      <Content $cardType={cardType}>
         <DetailContainer>
           <Detail>
             {cardType === 'default' && serviceType === 'work' && company && (
               <p className="text-xs">{company}</p>
             )}
-            <Title cardType={cardType}>{title}</Title>
+            <Title $cardType={cardType}>{title}</Title>
           </Detail>
           {cardType === 'default' && (
-            <div>{inWishlist ? <FullHeart /> : <Heart />}</div>
+            <div>{inWishlist ? <ColorHeart /> : <Heart />}</div>
           )}
         </DetailContainer>
-        <InfoContainer cardType={cardType} serviceType={serviceType}>
+        <InfoContainer $cardType={cardType} $serviceType={serviceType}>
           <BottomContainer>
             {serviceType === 'work' && company && (
-              <Price cardType={cardType}>시급 {priceToString(price)}원</Price>
+              <Price $cardType={cardType}>
+                시급 {price.toLocaleString('ko-KR')}원
+              </Price>
             )}
             {serviceType === 'default' && cardType === 'map' && (
-              <Price cardType={cardType}>{priceToString(price)}원~</Price>
+              <Price $cardType={cardType}>
+                {price.toLocaleString('ko-KR')}원~
+              </Price>
             )}
             {cardType === 'map' && (
-              <div>{inWishlist ? <FullHeart /> : <Heart />}</div>
+              <div>{inWishlist ? <ColorHeart /> : <Heart />}</div>
             )}
           </BottomContainer>
-          <Location cardType={cardType}>{location}</Location>
+          <Location $cardType={cardType}>{location}</Location>
         </InfoContainer>
       </Content>
     </Container>
--- frontend/src/utils/priceToString.ts
+++
@@ -1,3 +0,0 @@
-export default function priceToString(price: number) {
-  return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-}
