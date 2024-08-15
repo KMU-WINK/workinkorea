afe81533e35d59e2cbbed0c721088607b8b4c88a
j-myeong <jmyeong012@gmail.com>
2024-08-15 20:19:10 +0900

    feat: Disabled 구현 - 메인화면에서 컴포넌트 사용 시, 버튼이 될 예정이라 disabled props 추가 - width 옵션 수정 (w-full)

--- frontend/src/components/Input/index.tsx
+++ frontend/src/components/Input/index.tsx
@@ -3,24 +3,34 @@ import React from 'react';
 interface InputProps {
   leftIcon?: React.ReactNode;
   rightIcon?: React.ReactNode;
-  onChange?: (value: string) => void;
   placeholder?: string;
+  disabled?: boolean;
+  onChange?: (value: string) => void;
+  onClick?: () => void;
 }
 
 export default function Input({
   leftIcon,
   rightIcon,
   onChange,
+  onClick,
   placeholder = '검색어',
+  disabled = false,
 }: InputProps) {
   return (
-    <div className="flex gap-2.5 w-[345px] bg-white border border-gray-2 rounded-[10px] px-3.5 py-[18px]">
+    <div
+      className={`flex gap-2.5 w-full bg-white border border-gray-2 rounded-[10px] px-3.5 py-[18px] ${disabled && 'bg-gray-2'}`}
+      onClick={disabled ? onClick : undefined}
+      onKeyDown={disabled ? onClick : undefined}
+      role="button"
+      tabIndex={0}
+    >
       {leftIcon}
       <input
+        className="bg-transparent outline-none flex-1"
         type="text"
         placeholder={placeholder}
         onChange={e => onChange && onChange(e.target.value)}
-        className="outline-none flex-1"
       />
       {rightIcon}
     </div>
