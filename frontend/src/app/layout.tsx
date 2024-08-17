3f22fb2c35925e5dce192fd686d656038750c3d9
gun <heegun46900@gmail.com>
2024-08-17 14:32:38 +0900

    feat: layout.tsx - html 태그 배경 수정

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="bg-white">
       <body className={myFont.className}>
         <StyledJsxRegistry>{children}</StyledJsxRegistry>
       </body>
