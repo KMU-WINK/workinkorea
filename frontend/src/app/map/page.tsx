244358fdb30e30657f2bf2d64afe59a831b4c852
jisung <cham0810@kookmin.ac.kr>
2024-08-19 19:45:45 +0900

    feat: kakao map marker 생성

--- frontend/src/app/map/page.tsx
+++ frontend/src/app/map/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import Card from '@/components/Card';
 import { CardProps } from '@/types/type';
 
@@ -39,16 +39,73 @@ const testData: CardProps[] = [
 
 export default function Map() {
   const mapRef = useRef<HTMLDivElement>(null);
+  const markerImageSrc = '/svgs/ping.svg';
+  const activeMarkerImageSrc = '/svgs/ping-active.svg';
+  const activeMarkerRef = useRef<any>(null);
+
   useEffect(() => {
     window.kakao.maps.load(() => {
+      // 마커 샘플 데이터
+      const positions = [
+        {
+          title: '카카오',
+          latlng: new window.kakao.maps.LatLng(33.450705, 126.570677),
+        },
+        {
+          title: '생태연못',
+          latlng: new window.kakao.maps.LatLng(33.450936, 126.569477),
+        },
+        {
+          title: '텃밭',
+          latlng: new window.kakao.maps.LatLng(33.450879, 126.56994),
+        },
+        {
+          title: '근린공원',
+          latlng: new window.kakao.maps.LatLng(33.451393, 126.570738),
+        },
+      ];
+
       const options = {
+        // 지도 중심 좌표
         center: new window.kakao.maps.LatLng(33.450701, 126.570667),
         level: 3,
       };
 
       const map = new window.kakao.maps.Map(mapRef.current, options);
+
+      // 마커 이미지
+      const markerImage = new window.kakao.maps.MarkerImage(
+        markerImageSrc,
+        new window.kakao.maps.Size(20, 20),
+      );
+      const activeMarkerImage = new window.kakao.maps.MarkerImage(
+        activeMarkerImageSrc,
+        new window.kakao.maps.Size(36, 43),
+      );
+
+      // 여러개 마커 생성
+      positions.forEach(position => {
+        const marker = new window.kakao.maps.Marker({
+          map,
+          position: position.latlng,
+          title: position.title,
+          image: markerImage,
+          clickable: true,
+        });
+
+        window.kakao.maps.event.addListener(marker, 'click', () => {
+          if (!activeMarkerRef.current || activeMarkerRef.current !== marker) {
+            if (activeMarkerRef.current)
+              activeMarkerRef.current.setImage(markerImage);
+            // 마커 클릭시 active
+            marker.setImage(activeMarkerImage);
+          }
+          activeMarkerRef.current = marker;
+        });
+      });
     });
   }, []);
+
   return (
     <div>
       <div ref={mapRef} className="w-screen h-screen" />
