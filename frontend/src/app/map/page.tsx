8115796ad439f77ae34aaf272628908bfe698eda
jisung <cham0810@kookmin.ac.kr>
2024-08-26 15:01:56 +0900

    feat: 카드 슬라이드시 마커 자동 활성화 기능

--- frontend/src/app/map/page.tsx
+++ frontend/src/app/map/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
@@ -16,52 +16,37 @@ declare global {
 
 const testData: CardProps[] = [
   {
+    id: 1,
     cardType: 'map',
     serviceType: 'default',
     title: '홍대점',
     location: '연희동',
     image: '/',
     price: 5000,
+    onCardClick: () => {},
+    onWishListClick: () => {},
   },
   {
+    id: 2,
     cardType: 'map',
     serviceType: 'default',
     title: '홍대점',
     location: '연희동',
     image: '/',
     price: 5000,
+    onCardClick: () => {},
+    onWishListClick: () => {},
   },
   {
+    id: 3,
     cardType: 'map',
     serviceType: 'default',
     title: '홍대점',
     location: '연희동',
     image: '/',
     price: 5000,
-  },
-  {
-    cardType: 'map',
-    serviceType: 'default',
-    title: '홍대점',
-    location: '연희동',
-    image: '/',
-    price: 5000,
-  },
-  {
-    cardType: 'map',
-    serviceType: 'default',
-    title: '홍대점',
-    location: '연희동',
-    image: '/',
-    price: 5000,
-  },
-  {
-    cardType: 'map',
-    serviceType: 'default',
-    title: '홍대점',
-    location: '연희동',
-    image: '/',
-    price: 5000,
+    onCardClick: () => {},
+    onWishListClick: () => {},
   },
 ];
 
@@ -75,14 +60,40 @@ const SliderContainer = styled.div`
   }
 `;
 
+type MappingData = {
+  [index: number]: any;
+};
+
 export default function Map() {
   const mapRef = useRef<HTMLDivElement>(null);
   const markerImageSrc = '/svgs/ping.svg';
   const activeMarkerImageSrc = '/svgs/ping-active.svg';
   const activeMarkerRef = useRef<any>(null);
+  const mapObjectRef = useRef<any>(null);
+
+  const [mappingData, setMappingData] = useState<MappingData>({});
 
   const onSlideChange = (index: number) => {
-    console.log(index);
+    const currentMarker = mappingData[Math.round(index) + 1];
+    // 마커 이미지
+    const markerImage = new window.kakao.maps.MarkerImage(
+      markerImageSrc,
+      new window.kakao.maps.Size(20, 20),
+    );
+    const activeMarkerImage = new window.kakao.maps.MarkerImage(
+      activeMarkerImageSrc,
+      new window.kakao.maps.Size(36, 43),
+    );
+
+    // 그 전 마커 비활성화
+    if (activeMarkerRef.current) activeMarkerRef.current.setImage(markerImage);
+
+    // 현재 마커 좌표로 중심 이동
+    mapObjectRef.current.setCenter(currentMarker.getPosition());
+
+    // 현재 마커 활성화
+    currentMarker.setImage(activeMarkerImage);
+    activeMarkerRef.current = currentMarker;
   };
 
   const sliderSettings = {
@@ -102,18 +113,22 @@ export default function Map() {
       // 마커 샘플 데이터
       const positions = [
         {
+          id: 1,
           title: '카카오',
           latlng: new window.kakao.maps.LatLng(33.450705, 126.570677),
         },
         {
+          id: 2,
           title: '생태연못',
           latlng: new window.kakao.maps.LatLng(33.450936, 126.569477),
         },
         {
+          id: 3,
           title: '텃밭',
           latlng: new window.kakao.maps.LatLng(33.450879, 126.56994),
         },
         {
+          id: 4,
           title: '근린공원',
           latlng: new window.kakao.maps.LatLng(33.451393, 126.570738),
         },
@@ -126,6 +141,7 @@ export default function Map() {
       };
 
       const map = new window.kakao.maps.Map(mapRef.current, options);
+      mapObjectRef.current = map;
 
       // 마커 이미지
       const markerImage = new window.kakao.maps.MarkerImage(
@@ -147,12 +163,18 @@ export default function Map() {
           clickable: true,
         });
 
+        // 마커 객체 저장
+        setMappingData(prevData => ({
+          ...prevData,
+          [position.id]: marker,
+        }));
+
+        // 마커 클릭 이벤트
         window.kakao.maps.event.addListener(marker, 'click', () => {
           if (!activeMarkerRef.current || activeMarkerRef.current !== marker) {
             if (activeMarkerRef.current)
-              activeMarkerRef.current.setImage(markerImage);
-            // 마커 클릭시 active
-            marker.setImage(activeMarkerImage);
+              activeMarkerRef.current.setImage(markerImage); // 그 전 마커 비활성화
+            marker.setImage(activeMarkerImage); // 클릭된 마커 활성화
           }
           activeMarkerRef.current = marker;
         });
@@ -165,15 +187,20 @@ export default function Map() {
       <div className="max-w-sm h-full overflow-hidden relative">
         <div ref={mapRef} className="w-screen h-full" />
         <SliderContainer>
+          {/* eslint-disable-next-line react/jsx-props-no-spreading */}
           <Slider {...sliderSettings}>
             {testData.map(list => (
               <Card
+                key={list.id}
+                id={list.id}
                 cardType={list.cardType}
                 serviceType={list.serviceType}
                 title={list.title}
                 location={list.location}
                 image={list.image}
                 price={list.price}
+                onCardClick={list.onCardClick}
+                onWishListClick={list.onWishListClick}
               />
             ))}
           </Slider>
