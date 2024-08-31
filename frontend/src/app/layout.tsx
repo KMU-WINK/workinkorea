7c0d184adbb4a56b261772bddfb129b31043ff67
Jiwon Choi <jwchoi@lablup.com>
2024-08-31 17:15:38 +0900

    feat: open graph 이미지 추가

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -15,11 +15,10 @@ export const metadata: Metadata = {
     title: '워크인코리아',
     description: '원하는 곳에서 머무르며 일하다',
     type: 'website',
-    url: 'https://127.0.0.1:3000', // 사이트 배포 이후 수정 필요
+    url: 'https://workinkorea.vercel.app',
     images: [
-      // 디자인 요청 필요
       {
-        url: 'https://example.com/og.png',
+        url: '/images/ogImage.png',
         type: 'image/png',
         width: '1200',
         height: '630',
