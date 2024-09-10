522b16c655a31a5bc81d0f4efd122c42db668dbc
Jiwon Choi <jwchoi@lablup.com>
2024-09-10 23:28:36 +0900

    fix: favicon 미노출 에러 수정

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -20,8 +20,9 @@ export const metadata: Metadata = {
   robots: 'index, follow',
   manifest: '/manifest.json',
   icons: [
-    { rel: 'apple-touch-icon', url: 'icons/icon-128x128.png' },
-    { rel: 'icon', url: 'icons/icon-128x128.png' },
+    { rel: 'favicon', url: '/favicon.ico' },
+    { rel: 'apple-touch-icon', url: '/icons/icon-128x128.png' },
+    { rel: 'icon', url: '/icons/icon-128x128.png' },
   ],
   openGraph: {
     locale: 'ko_KR',
