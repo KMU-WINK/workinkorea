4a7c71ff374c45945fc7d24c97410865c0be0317
Jiwon Choi <jwchoi@lablup.com>
2024-09-28 12:52:28 +0900

    feat: 페이지 예외 처리 추가(에러 페이지, 미존재 페이지)

---
+++ frontend/src/app/not-found.tsx
@@ -0,0 +1,7 @@
+import ExceptionPageTemplate from '@/components/ExceptionPage/page-template';
+
+const NotFound = () => {
+  return <ExceptionPageTemplate alertText="페이지를 불러올 수 없어요" />;
+};
+
+export default NotFound;
